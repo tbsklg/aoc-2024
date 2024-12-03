a5562a2fb4f13a4eec9753bdf728fe8043ca9926
Tobias Klug <tbs.klg@gmail.com>
2024-12-03 13:42:24 +0100

    Solve part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("Part 1: {}", day3::part1::solve(&input));
+    println!("Part 2: {}", day3::part2::solve(&input));
 }
--- day3/src/part2.rs
+++ day3/src/part2.rs
@@ -1,6 +1,27 @@
-use std::iter::zip;
+use regex::Regex;
+
+use crate::part1;
 
 pub fn solve(input: &str) -> usize {
-    3
-}
+    let re = Regex::new(r"mul\((\d+,\d+\))|do\(\)|don't\(\)").unwrap();
 
+    let mut captures = vec![];
+    let mut capture = true;
+    for cap in re.captures_iter(input) {
+        if &cap[0] == "do()" {
+            capture = true;
+            continue;
+        }
+
+        if &cap[0] == "don't()" {
+            capture = false;
+            continue;
+        }
+
+        if capture {
+            captures.push(cap[0].to_owned());
+        }
+    }
+    
+    part1::solve(&captures.concat())
+}
