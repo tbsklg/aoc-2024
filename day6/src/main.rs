590e7b7d7a5fb926a2536cceaf1e65351df80a9e
Tobias Klug <tbs.klg@gmail.com>
2024-12-09 21:10:31 +0100

    Solve part 1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
@@ -5,9 +7,7 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
-    let map = Map::from(input);
-    println!("{:#?}", map);
-    1
+    Map::from(input).walk()
 }
 
 #[derive(Debug)]
@@ -16,6 +16,25 @@ enum Dir {
     Down,
     Left,
     Right,
+}
+impl Dir {
+    fn next(&self, (row, col): (i32, i32)) -> (i32, i32) {
+        match self {
+            Dir::Up => (row - 1, col),
+            Dir::Down => (row + 1, col),
+            Dir::Left => (row, col - 1),
+            Dir::Right => (row, col + 1),
+        }
+    }
+
+    fn turn(&self) -> Dir {
+        match self {
+            Dir::Up => Dir::Right,
+            Dir::Down => Dir::Left,
+            Dir::Left => Dir::Up,
+            Dir::Right => Dir::Down,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -27,29 +46,48 @@ struct Guard {
 #[derive(Debug)]
 struct Map {
     guard: Guard,
-    obstructions: Vec<(usize, usize)>,
+    grid: Vec<Vec<char>>,
 }
 
-type Pos = (usize, usize);
+type Pos = (i32, i32);
 
 impl From<&str> for Map {
     fn from(input: &str) -> Self {
         let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
         let (rows, cols) = (grid.len(), grid.first().map_or(0, |l| l.len()));
 
-        let obstructions = (0..rows)
-            .flat_map(|row| (0..cols).map(move |col| (row, col)))
-            .filter(|&(row, col)| grid[row][col] == '#')
-            .collect();
-
         let pos = (0..rows)
             .flat_map(|row| (0..cols).map(move |col| (row, col)))
             .find(|&(row, col)| grid[row][col] == '^')
+            .map(|(row, col)| (row as i32, col as i32))
             .expect("No guard found!");
 
         Self {
             guard: Guard { pos, dir: Dir::Up },
-            obstructions,
+            grid,
         }
     }
+}
+
+impl Map {
+    fn walk(&mut self) -> usize {
+        let mut points: HashSet<Pos> = HashSet::new();
+
+        loop {
+            points.insert(self.guard.pos);
+            let next = self.guard.dir.next(self.guard.pos);
+            
+            match self.get(next) {
+                Some('#') => self.guard.dir = self.guard.dir.turn(),
+                Some(_) => self.guard.pos = next,
+                None => break,
+            }
+        }
+
+        points.len()
+    }
+
+    fn get(&self, (row, col): Pos) -> Option<char> {
+        self.grid.get(row as usize).and_then(|r| r.get(col as usize).copied())
+    } 
 }
