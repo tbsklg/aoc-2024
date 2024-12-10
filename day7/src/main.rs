74ba6534cdbbe7e0a055158a38aef7f3dc1bad28
Tobias Klug <tbs.klg@gmail.com>
2024-12-10 22:15:12 +0100

    Solve part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -11,6 +12,40 @@ fn part1(input: &str) -> usize {
         .filter(|v| is_correct_equation(*v))
         .map(|v| v.0)
         .sum()
+}
+
+fn part2(input: &str) -> usize {
+    Calibration::from(input)
+        .equations
+        .iter()
+        .filter(|v| is_correct_equation_2(*v))
+        .map(|v| v.0)
+        .sum()
+}
+
+fn is_correct_equation_2(input: &(usize, Vec<usize>)) -> bool {
+    fn find(v: &[usize], target: usize, current: usize) -> bool {
+        if current > target {
+            return false;
+        }
+
+        if v.is_empty() {
+            return current == target;
+        }
+
+        let head = v.first().unwrap();
+        let tail = &v[1..];
+
+        find(tail, target, current + head)
+            || find(tail, target, current * head)
+            || find(tail, target, concat(current, *head))
+    }
+
+    find(&input.1, input.0, 0)
+}
+
+fn concat(a: usize, b: usize) -> usize {
+    format!("{}{}", a, b).parse().unwrap()
 }
 
 fn is_correct_equation(input: &(usize, Vec<usize>)) -> bool {
@@ -33,6 +68,12 @@ fn is_correct_equation(input: &(usize, Vec<usize>)) -> bool {
 }
 
 type Equation = (usize, Vec<usize>);
+
+enum Operator {
+    Add,
+    Multi,
+    Concat,
+}
 
 #[derive(Debug)]
 struct Calibration {
