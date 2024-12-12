dd9f976e25471ce6a262fd5ac3cfc6427303b96e
Tobias Klug <tbs.klg@gmail.com>
2024-12-12 21:26:29 +0100

    WIP: part one

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -5,57 +5,65 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
-    let disk = Disk::from(input);
+    let disk = &mut Disk::from(input);
+    disk.compress();
     println!("{:?}", disk.data);
-    println!("{:?}", disk.free_space);
     1
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Data {
-    id: u32,
+    id: Option<u32>,
     offset: u32,
     len: u32,
 }
 
-#[derive(Debug)]
-struct Space {
-    offset: u32,
-    len: u32,
+impl Data {
+    fn update_len(&mut self, len: u32) {
+        self.len = len
+    }
 }
 
 #[derive(Debug)]
 struct Disk {
     data: Vec<Data>,
-    free_space: Vec<Space>,
 }
 
 impl From<&str> for Disk {
     fn from(input: &str) -> Self {
         let mut data: Vec<Data> = vec![];
-        let mut free_space: Vec<Space> = vec![];
 
         let mut id = 0..;
         let mut raw_data = input.trim_end().chars().map(|x| x.to_digit(10)).enumerate();
 
         let mut offset = 0;
         while let Some((i, v)) = raw_data.next() {
+            let len = v.unwrap_or(0);
             if is_even(&i) {
                 data.push(Data {
-                    id: id.next().unwrap(),
+                    id: Some(id.next().unwrap()),
                     offset,
-                    len: v.unwrap_or(0),
+                    len,
                 });
             } else {
-                free_space.push(Space {
+                data.push(Data {
+                    id: None,
                     offset,
-                    len: v.unwrap_or(0),
+                    len,
                 })
             }
-            offset += v.unwrap_or(0);
+            offset += len;
         }
 
-        Self { data, free_space }
+        Self { data }
+    }
+}
+
+impl Disk {
+    fn compress(&mut self) {
+        let spaces = 
+            self.data.iter().filter(|d| d.id.is_none()).collect::<Vec<&Data>>();
+
     }
 }
 
