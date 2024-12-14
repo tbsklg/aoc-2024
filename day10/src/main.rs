b7998aaf947872b7e21a2795b889b61317e03b4a
Tobias Klug <tbs.klg@gmail.com>
2024-12-14 13:55:16 +0100

    Solve part one

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
@@ -5,8 +7,8 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
-    let map = Map::from(input);
-    println!("{:?}", map.get((0,3)));
+    let tracks = Map::from(input).count_tracks();
+    println!("{:?}", tracks);
     1
 }
 
@@ -30,7 +32,61 @@ impl From<&str> for Map {
 }
 
 impl Map {
-    fn get(&self, (r, c): (usize, usize)) -> Option<u8> {
-        self.topographic.get(r).and_then(|l| l.get(c)).copied()
+    fn get(&self, (r, c): (i32, i32)) -> Option<u8> {
+        self.topographic
+            .get(r as usize)
+            .and_then(|l| l.get(c as usize))
+            .copied()
     }
+
+    fn count_tracks(&self) -> usize {
+        self.topographic
+            .iter()
+            .enumerate()
+            .map(|(i, l)| {
+                l.iter()
+                    .enumerate()
+                    .filter(|(_, p)| **p == 0)
+                    .map(move |(j, _)| (i as i32, j as i32))
+                    .map(|p| self.find_tracks(p))
+                    .sum::<usize>()
+            })
+            .sum()
+    }
+
+    fn find_tracks(&self, position: (i32, i32)) -> usize {
+        let mut stack = vec![];
+        let mut visited = HashSet::new();
+
+        let mut found = 0;
+        stack.push((position, 0));
+
+        while let Some((p, c)) = stack.pop() {
+            if visited.insert(p) {
+                if c == 9 {
+                    found += 1;
+                    continue;
+                }
+            }
+
+            for n in neighbors(&p) {
+                match self.get(n) {
+                    Some(x) => {
+                        if x == self.get(p).unwrap() + 1 {
+                            stack.push((n, c + 1))
+                        }
+                    }
+                    None => continue,
+                }
+            }
+        }
+
+        found
+    }
+}
+
+fn neighbors((r, c): &(i32, i32)) -> Vec<(i32, i32)> {
+    let dirs: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, -1), (0, 1)];
+
+    dirs.iter().map(|(x, y)| (r + x, c + y)).collect()
 }
