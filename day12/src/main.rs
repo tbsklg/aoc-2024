b623df96fd35c1605d6dcb5424b9c371234897f4
Tobias Klug <tbs.klg@gmail.com>
2024-12-19 15:59:04 +0100

    Day 12 - 2 (#14)

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -4,10 +4,15 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
     calculate_price(Map::from(input).find_areas())
+}
+
+fn part2(input: &str) -> usize {
+    calculate_price_for_sides(Map::from(input).find_areas())
 }
 
 struct Map {
@@ -30,14 +35,26 @@ impl From<&str> for Map {
     }
 }
 
-fn calculate_price(areas: Vec<(char, usize, usize)>) -> usize {
+fn calculate_price(areas: Vec<Area>) -> usize {
     areas
         .iter()
-        .fold(0, |acc, (_, size, perimeter)| acc + size * perimeter)
+        .fold(0, |acc, area| acc + area.points.len() * area.perimeter)
+}
+
+fn calculate_price_for_sides(areas: Vec<Area>) -> usize {
+    areas
+        .iter()
+        .map(|area| area.points.len() * sides(&area.points.iter().copied().collect::<Vec<_>>()))
+        .sum()
+}
+
+struct Area {
+    points: HashSet<(i32, i32)>,
+    perimeter: usize,
 }
 
 impl Map {
-    fn find_areas(&self) -> Vec<(char, usize, usize)> {
+    fn find_areas(&self) -> Vec<Area> {
         (0..self.rows as i32)
             .flat_map(|r| (0..self.cols as i32).map(move |c| (r, c)))
             .fold((HashSet::new(), Vec::new()), |(mut acc, mut areas), p| {
@@ -46,15 +63,15 @@ impl Map {
                 }
 
                 let area = self.find_area(p);
-                acc.extend(area.0.iter().copied());
-                areas.push((self.get(&p).unwrap(), area.0.len(), area.1));
+                acc.extend(area.points.iter().copied());
+                areas.push(area);
 
                 (acc, areas)
             })
             .1
     }
 
-    fn find_area(&self, position: (i32, i32)) -> (HashSet<(i32, i32)>, usize) {
+    fn find_area(&self, position: (i32, i32)) -> Area {
         let mut visited = HashSet::new();
         let mut stack = vec![position];
         let mut perimeter: usize = 0;
@@ -65,7 +82,6 @@ impl Map {
             }
 
             let neighbors = self.neighbors(&p);
-
             perimeter += 4 - neighbors.len();
 
             for n in neighbors {
@@ -73,7 +89,10 @@ impl Map {
             }
         }
 
-        (visited, perimeter)
+        Area {
+            points: visited,
+            perimeter,
+        }
     }
 
     fn neighbors(&self, (r, c): &(i32, i32)) -> Vec<(i32, i32)> {
@@ -98,5 +117,165 @@ impl Map {
 
     fn in_bounds(&self, (r, c): &(i32, i32)) -> bool {
         *r >= 0 && *r < self.rows as i32 && *c >= 0 && *c < self.cols as i32
+    }
+}
+
+#[derive(PartialEq)]
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
+fn directions(point: &(i32, i32), points: &[(i32, i32)]) -> Vec<Direction> {
+    [
+        ((point.0 + 1, point.1), Direction::Down),
+        ((point.0 - 1, point.1), Direction::Up),
+        ((point.0, point.1 + 1), Direction::Right),
+        ((point.0, point.1 - 1), Direction::Left),
+    ]
+    .into_iter()
+    .filter_map(|(p, d)| if points.contains(&p) { Some(d) } else { None })
+    .collect()
+}
+
+fn sides(points: &[(i32, i32)]) -> usize {
+    let mut sides = 0;
+
+    for point in points {
+        let directions = directions(point, points);
+
+        if directions.is_empty() {
+            sides += 4;
+            continue;
+        }
+
+        if directions.len() == 1 {
+            sides += 2;
+            continue;
+        }
+
+        let (top, left, right, bottom) = (
+            directions.contains(&Direction::Up),
+            directions.contains(&Direction::Left),
+            directions.contains(&Direction::Right),
+            directions.contains(&Direction::Down),
+        );
+        
+        // Todo: Refactor this
+        match (bottom, left, right) {
+            (true, true, true) => {
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 + 1 && p.1 == point.1 - 1)
+                    .map_or(1, |_| 0);
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 + 1 && p.1 == point.1 + 1)
+                    .map_or(1, |_| 0);
+            }
+            (true, true, false) => {
+                // - 1
+                sides += if top { 0 } else { 1 };
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 + 1 && p.1 == point.1 - 1)
+                    .map_or(1, |_| 0);
+            }
+            (true, false, true) => {
+                // + 1
+                sides += if top { 0 } else { 1 };
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 + 1 && p.1 == point.1 + 1)
+                    .map_or(1, |_| 0);
+            }
+            _ => (),
+        };
+
+        match (top, left, right) {
+            (true, true, true) => {
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 - 1 && p.1 == point.1 + 1)
+                    .map_or(1, |_| 0);
+
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 - 1 && p.1 == point.1 - 1)
+                    .map_or(1, |_| 0);
+            }
+            (true, true, false) => {
+                sides += if bottom { 0 } else { 1 };
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 - 1 && p.1 == point.1 - 1)
+                    .map_or(1, |_| 0);
+            }
+            (true, false, true) => {
+                sides += if bottom { 0 } else { 1 };
+                sides += points
+                    .iter()
+                    .find(|p| p.0 == point.0 - 1 && p.1 == point.1 + 1)
+                    .map_or(1, |_| 0);
+            }
+            _ => (),
+        }
+    }
+    sides
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part2, sides};
+
+    #[test]
+    fn should_calculate_sides_when_no_points() {
+        let points = vec![];
+
+        assert_eq!(0, sides(&points));
+    }
+
+    #[test]
+    fn should_calculate_when_only_horizontal() {
+        let points = vec![(0, 0), (0, 1), (0, 2)];
+
+        assert_eq!(4, sides(&points));
+    }
+
+    #[test]
+    fn should_calculate_when_bottom_right() {
+        let points = vec![(0, 0), (0, 1), (0, 2), (1, 0)];
+
+        assert_eq!(6, sides(&points));
+    }
+
+    #[test]
+    fn should_calculate_when_bottom_right_and_bottom_left() {
+        let points = vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 2)];
+
+        assert_eq!(8, sides(&points));
+    }
+
+    #[test]
+    fn should_calculate_sides_when_top_right() {
+        let points = vec![(1, 0), (2, 0), (2, 1), (2, 2)];
+
+        assert_eq!(6, sides(&points));
+    }
+
+    #[test]
+    fn should_calculate_sides_when_top_right_and_top_left() {
+        let points = vec![(1, 0), (2, 0), (2, 1), (2, 2), (1, 2)];
+
+        assert_eq!(8, sides(&points));
+    }
+
+    #[test]
+    fn calculate_price_for_sides() {
+        let puzzle = "AAAAAA\nAAABBA\nAAABBA\nABBAAA\nABBAAA\nAAAAAA";
+
+        assert_eq!(368, part2(puzzle));
     }
 }
