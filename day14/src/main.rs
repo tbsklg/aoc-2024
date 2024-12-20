1466442787576519ab344d5b48d60b8b72ba0417
Tobias Klug <tbs.klg@gmail.com>
2024-12-20 16:18:01 +0100

    Simulate points

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 fn main() {
@@ -7,11 +9,35 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
+    let robots: HashMap<(isize, isize), usize> = create_robots(input)
+        .iter()
+        .map(|r| simulate(r, 100))
+        .fold(HashMap::new(), |mut acc, c| {
+            acc.entry(c).and_modify(|c| *c += 1).or_insert(1);
+            acc
+        });
+
+    println!("{:?}", robots);
     0
+}
+
+const WIDTH: isize = 11;
+const HEIGHT: isize = 7;
+
+fn create_robots(input: &str) -> Vec<Robot> {
+    input.lines().map(Robot::from).collect::<Vec<Robot>>()
+}
+
+fn simulate(r: &Robot, times: isize) -> (isize, isize) {
+    let (rx, ry) = r.pos;
+    let (vx, vy) = r.vel;
+
+    ((rx + 1 + vx * times).rem_euclid(WIDTH), (ry + 1 + vy * times).rem_euclid(HEIGHT))
 }
 
 #[derive(Debug, PartialEq)]
 struct Robot {
+    // x, y
     pos: (isize, isize),
     vel: (isize, isize),
 }
@@ -35,10 +61,16 @@ impl From<&str> for Robot {
 mod tests {
     use crate::Robot;
 
-    #[test] 
+    #[test]
     fn should_create_robot_from_line() {
         let line = "p=0,4 v=3,-3";
-        
-        assert_eq!(Robot { pos: (0, 4), vel: (3, -3) }, Robot::from(line))
+
+        assert_eq!(
+            Robot {
+                pos: (0, 4),
+                vel: (3, -3)
+            },
+            Robot::from(line)
+        )
     }
 }
