d1a15403aef1852872a2341d3859f40d52fd39b3
Tobias Klug <tbs.klg@gmail.com>
2024-12-23 09:03:51 +0100

    WIP: part one

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,17 +1,15 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    
+
     println!("Part 1: {}", part1(&input));
 }
 
 fn part1(input: &str) -> usize {
     let instructions: Vec<&str> = input.split("\n\n").collect();
-    let map = Map::from(instructions[0]);
+    let map = &mut Map::from(instructions[0]);
     let moves: Vec<Dir> = instructions[1].trim_end().chars().map(Dir::from).collect();
 
-    println!("{:?}", map);
-    println!("{:?}", moves);
-
+    map.move_robot(moves);
 
     0
 }
@@ -36,10 +34,30 @@ impl From<char> for Dir {
     }
 }
 
-#[derive(Debug)]
+impl Dir {
+    fn next(&self, (l, t): (usize, usize)) -> (usize, usize) {
+        match self {
+            Dir::Up => (l, t - 1),
+            Dir::Left => (l - 1, t),
+            Dir::Right => (l + 1, t),
+            Dir::Down => (l, t + 1),
+        }
+    }
+
+    fn step(&self) -> (i32, i32) {
+        match self {
+            Dir::Up => (0, -1),
+            Dir::Left => (-1, 0),
+            Dir::Right => (1, 0),
+            Dir::Down => (0, 1),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 struct Map {
     warehouse: Vec<Vec<char>>,
-    robot: (isize, isize),
+    robot: (usize, usize),
 }
 
 impl From<&str> for Map {
@@ -49,7 +67,7 @@ impl From<&str> for Map {
         let robot = warehouse.iter().enumerate().find_map(|(row, line)| {
             line.iter().enumerate().find_map(|(col, &c)| {
                 if c == '@' {
-                    Some((row as isize, col as isize))
+                    Some((row as usize, col as usize))
                 } else {
                     None
                 }
@@ -64,7 +82,65 @@ impl From<&str> for Map {
 }
 
 impl Map {
-    fn move_robot(&self, moves: Vec<Dir>) -> Vec<char> {
-        todo!();
+    fn move_robot(&mut self, moves: Vec<Dir>) {
+        for m in moves {
+            let next_robot_position = m.next(self.robot);
+
+            match self.get(next_robot_position) {
+                Some('#') => continue,
+                Some('.') => {
+                    self.set(self.robot, '.');
+                    self.set(next_robot_position, '@');
+                    self.robot = next_robot_position;
+                    println!("Robot moved to {:?}", next_robot_position);
+                    self.print_warehouse();
+                    continue;
+                }
+                Some('O') => {
+                    self.try_to_move_box(next_robot_position, m.step());
+                    self.print_warehouse();
+                }
+                _ => continue,
+            }
+
+        }
+    }
+    
+    fn try_to_move_box(&mut self, box_position: (usize, usize), step: (i32, i32)) {
+        let next_box_position = (box_position.0 as i32 + step.0, box_position.1 as i32 + step.1);
+        let next_box_position = (next_box_position.0 as usize, next_box_position.1 as usize);
+
+        loop {
+            match self.get(next_box_position) {
+                Some('#') => break,
+                Some('.') => {
+                    self.set(box_position, '.');
+                    self.set(next_box_position, 'O');
+                    break;
+                }
+                _ => {
+                    self.try_to_move_box(next_box_position, step);
+                    self.set(box_position, '.');
+                    self.set(next_box_position, 'O');
+                    break;
+                }
+            }
+        }
+
+    }
+
+    fn get(&self, (l, t): (usize, usize)) -> Option<char> {
+        self.warehouse.get(t).and_then(|row| row.get(l)).copied()
+    }
+
+    fn set(&mut self, (l, t): (usize, usize), c: char) {
+        self.warehouse[t][l] = c;
+    }
+
+    fn print_warehouse(&self) {
+        self.warehouse.iter().for_each(|row| {
+            row.iter().for_each(|c| print!("{}", c));
+            println!();
+        });
     }
 }
