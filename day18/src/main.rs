4a14a07b0ba2de9eaaf5fb84e0499e4a5a483eaf
Tobias Klug <tbs.klg@gmail.com>
2024-12-30 14:23:47 +0100

    Solve part two

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,25 +1,45 @@
 use std::collections::{BinaryHeap, HashSet};
 
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("Part 1: {:?}", part1(&input));
+    println!("Part 2: {:?}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
-    let byte_positions = coordinates(input, 1024);
+    let byte_positions = coordinates(input);
     let start = State {
         pos: (0, 0),
         steps: 0,
     };
 
-    shortest_path(&byte_positions, start).unwrap()
+    shortest_path(&byte_positions[0..=1024].iter().cloned().collect(), start).unwrap()
 }
 
-fn coordinates(input: &str, n: usize) -> Vec<Pos> {
+fn part2(input: &str) -> Option<Pos> {
+    let byte_positions = coordinates(input);
+    let start = State {
+        pos: (0, 0),
+        steps: 0,
+    };
+
+    (1024..byte_positions.len())
+        .into_par_iter()
+        .find_map_first(|i| {
+            match shortest_path(&byte_positions[0..=i].iter().cloned().collect(), start) {
+                Some(_) => None,
+                None => Some(i),
+            }
+        })
+        .map(|i| byte_positions[i])
+}
+
+fn coordinates(input: &str) -> Vec<Pos> {
     input
         .lines()
-        .take(n)
         .map(|l| {
             let c = l
                 .split(",")
@@ -53,7 +73,7 @@ impl PartialOrd for State {
 const DIRS: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 const GRID_SIZE: i32 = 71;
 
-fn shortest_path(byte_positions: &Vec<Pos>, start: State) -> Option<usize> {
+fn shortest_path(byte_positions: &HashSet<Pos>, start: State) -> Option<usize> {
     let mut queue = BinaryHeap::from([start]);
     let mut seen: HashSet<Pos> = HashSet::new();
 
