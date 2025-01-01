3240627d2af117ff668d0c59d1a0aa742a39c0d0
Tobias Klug <tbs.klg@gmail.com>
2025-01-01 10:20:19 +0100

    Solve part two

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -13,6 +14,35 @@ fn part1(input: &str) -> usize {
         .lines()
         .filter(|design| is_valid(design, &patterns))
         .count()
+}
+
+fn part2(input: &str) -> usize {
+    let parts: Vec<&str> = input.split("\n\n").collect();
+    let patterns = extract_patterns(parts[0]);
+    let mut memo = HashMap::new();
+
+    parts[1]
+        .lines()
+        .map(|design| number_of_ways(design, &patterns, &mut memo))
+        .sum()
+}
+
+fn number_of_ways<'a>(design: &'a str, patterns: &HashSet<&str>, memo: &mut HashMap<&'a str, usize>) -> usize {
+    if design.is_empty() {
+        return 1;
+    }
+
+    if  memo.contains_key(design) {
+        return *memo.get(&design).unwrap();
+    }
+    
+    let design_count: usize = patterns.iter()
+        .map(|p| design.strip_prefix(p))
+        .map(|d| d.map_or(0, |nd| number_of_ways(nd, patterns, memo)))
+        .sum();
+    
+    memo.insert(design, design_count);
+    design_count
 }
 
 fn is_valid(design: &str, patterns: &HashSet<&str>) -> bool {
