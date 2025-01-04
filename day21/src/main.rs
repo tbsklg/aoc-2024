bb9d3dbb48efe8db04369111ae17f6eb078a6595
Tobias Klug <tbs.klg@gmail.com>
2025-01-04 10:55:43 +0100

    Solve part one using brute force

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,13 +1,14 @@
 use std::collections::VecDeque;
 
+use regex::Regex;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("Part 1: {}", part1(&input));
 }
 
-type NumPad = [[char; 3]; 4];
-type DirPad = [[char; 3]; 2];
+type Pad = Vec<Vec<char>>;
 type Pos = (i32, i32);
 type Dir = (i32, i32);
 
@@ -15,24 +16,47 @@ const DIRS: [Dir; 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
 
 fn part1(input: &str) -> usize {
     let codes = extract_codes(input);
-    let num_pad: NumPad = [
-        ['7', '8', '9'],
-        ['4', '5', '6'],
-        ['1', '2', '3'],
-        ['\0', '0', 'A'],
+    let num_pad: Pad = vec![
+        vec!['7', '8', '9'],
+        vec!['4', '5', '6'],
+        vec!['1', '2', '3'],
+        vec!['\0', '0', 'A'],
     ];
-    let dir_pad: DirPad = [['\0', '^', 'A'], ['<', 'v', '>']];
-
-    let paths = shortest_paths(&num_pad, (2, 3), "029A");
-
-    println!("{:?}", paths);
-    0
+    let dir_pad: Pad = vec![vec!['\0', '^', 'A'], vec!['<', 'v', '>']];
+    
+    complexity(&num_pad, &dir_pad, codes)
 }
 
-fn shortest_paths(num_pad: &[[char; 3]; 4], start: Pos, code: &str) -> Vec<Vec<char>> {
+fn complexity(num_pad: &Pad, dir_pad: &Pad, codes: Vec<&str>) -> usize {
+    codes
+        .iter()
+        .map(|code| extract_num(code) * translate_code(num_pad, dir_pad, code))
+        .sum()
+}
+
+fn extract_num(code: &str) -> usize {
+    let re = Regex::new(r"\d+").unwrap(); // Regular expression to match one or more digits
+
+    re.captures(code)
+        .and_then(|cap| cap.get(0)) // Get the first capture group
+        .and_then(|m| m.as_str().parse().ok()) // Parse it to a usize and handle any error
+        .unwrap_or(0) // Return 0 if no match is found or if parsing fails
+}
+
+fn translate_code(num_pad: &Pad, dir_pad: &Pad, code: &str) -> usize {
+    shortest_paths(&num_pad, (2, 3), code)
+        .iter()
+        .flat_map(|p| shortest_paths(&dir_pad, (2, 0), &p.into_iter().collect::<String>()))
+        .flat_map(|p| shortest_paths(&dir_pad, (2, 0), &p.into_iter().collect::<String>()))
+        .map(|p| p.len())
+        .min()
+        .unwrap_or(0)
+}
+
+fn shortest_paths(pad: &Vec<Vec<char>>, start: Pos, code: &str) -> Vec<Vec<char>> {
     code.chars()
         .fold((start, vec![vec![]]), |(start, all_paths), c| {
-            let (next_pos, paths) = shortest_path(num_pad, start, c);
+            let (next_pos, paths) = shortest_path(&pad, start, c);
 
             let new_paths = all_paths
                 .into_iter()
@@ -50,14 +74,14 @@ fn shortest_paths(num_pad: &[[char; 3]; 4], start: Pos, code: &str) -> Vec<Vec<c
         .1 // Extract
 }
 
-fn shortest_path(num_pad: &[[char; 3]; 4], start: Pos, end: char) -> (Pos, Vec<Vec<char>>) {
+fn shortest_path(pad: &Vec<Vec<char>>, start: Pos, end: char) -> (Pos, Vec<Vec<char>>) {
     let mut queue: VecDeque<(Pos, Vec<(Pos, Dir)>)> = VecDeque::from([(start, vec![])]);
     let mut paths: Vec<Vec<char>> = vec![];
     let mut min_path = usize::MAX;
     let mut end_pos = start;
 
     while let Some((curr, path)) = queue.pop_front() {
-        if get(num_pad, curr) == Some(end) {
+        if get(pad, curr) == Some(end) {
             if path.len() > min_path {
                 break;
             }
@@ -78,7 +102,7 @@ fn shortest_path(num_pad: &[[char; 3]; 4], start: Pos, end: char) -> (Pos, Vec<V
                 let next = (curr.0 + dir.0, curr.1 + dir.1);
                 let visited = path.iter().map(|p| p.0).collect::<Vec<_>>();
 
-                if !visited.contains(&next) && get(num_pad, next).is_some_and(|c| c != '\0') {
+                if !visited.contains(&next) && get(pad, next).is_some_and(|c| c != '\0') {
                     let mut next_path = path.clone();
                     next_path.push((next, *dir));
                     Some((next, next_path))
@@ -106,7 +130,7 @@ fn print_dir(dir: Dir) -> char {
     }
 }
 
-fn get(num_pad: &NumPad, pos: Pos) -> Option<char> {
+fn get(num_pad: &Pad, pos: Pos) -> Option<char> {
     num_pad
         .get(pos.1 as usize)
         .and_then(|l| l.get(pos.0 as usize))
