ce5a29fec018a9511df11bab38d7ced27c04ae6a
Tobias Klug <tbs.klg@gmail.com>
2025-01-08 11:13:37 +0100

    Initial setup

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    println!("Part 1: {}", part1(&input));
+}
+
+fn part1(input: &str) -> usize {
+    0
 }
