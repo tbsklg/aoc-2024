5abc32abdd4ec5583bcdf2ae321628dfa43b17e2
Tobias Klug <tbs.klg@gmail.com>
2025-01-09 16:54:22 +0100

    Solve part two

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 fn part1(input: &str) -> String {
 
     // Thanks to https://github.com/hyperneutrino for providing a solution
+    // see https://www.youtube.com/watch?v=y-UPxMAh2N8
     let register = &mut extract_register(input);
     let program = extract_program(input);
 
